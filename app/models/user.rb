d7bb39c8b4edc59bba1dea7bc0faef80cdf26038
Mandeep Khera <world.mover@gmail.com>
2011-06-11 21:54:02 +0100

    removed double yield in application

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
+
+	attr_accessor :password
 	attr_accessible :name, :email, :password, :password_confirmation
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 
-	validates :name, :presence => true
+	validates :name, :presence => true,
                         :length   => { :maximum => 50 } 
-	validates :email, :presence => true
-			    :format   => { :with => email_regex }
+	validates :email, :presence => true,
+			    :format   => { :with => email_regex },
 			    :uniqueness => true			
 
 	validates :password, :presence     => true,
@@ -20,7 +22,7 @@ class User < ActiveRecord::Base
 	encrypted_password == encrypt(submitted_password)
   end
 
- def self.authenticate(email, submitted_password)
+  def self.authenticate(email, submitted_password)
     user = find_by_email(email)
     return nil  if user.nil?
     return user if user.has_password?(submitted_password)
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Goals::Application.routes.draw do
-  get "sessions/new"
 
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/about',   :to => 'pages#about'
+
   match '/signup',  :to => 'users#new'
   match '/signin',  :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
 	
-  get "pages/home"
-  get "pages/about"
+  root :to => 'pages#home'
+
 
  end
