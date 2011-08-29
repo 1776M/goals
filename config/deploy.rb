58e0a68ac963e75df9833c788d4766042497c56c
Mandeep Khera <world.mover@gmail.com>
2011-08-29 13:37:42 +0100

    updating latest after long layoff

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,23 @@
 require 'bundler/capistrano'
 set :user, 'root'
-set :domain, 'reachmygoals.org'
+set :application, "reachmygoals.org"
 set :applicationdir, "application"
 
 set :application, "goals"
-set :repository,  "ssh://root@178.79.177.231/~/goals.git"
+set :repository,  "git@github.com:Polymatheus/first_app.git"
 
-set :scm, 'git'
+set :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "domain"                          # Your HTTP server, Apache/etc
-role :app, "domain"                          # This may be the same as your `Web` server
-role :db,  "domain", :primary => true # This is where Rails migrations will run
+role :web, "reachmygoals.org"                          # Your HTTP server, Apache/etc
+role :app, "reachmygoals.org"                          # This may be the same as your `Web` server
+role :db,  "reachmygoals.org", :primary => true # This is where Rails migrations will run
+
+set :user, 'deploy'
+set :use_sudo, false
+set :deploy_to, "/srv/www/#{application}"
+set :deploy_via, :remote_cache 
+
 
 
 # if you're still using the script/reaper helper you will need
@@ -21,6 +27,6 @@ role :db,  "domain", :primary => true # This is where Rails migrations will run
    task :start do ; end
    task :stop do ; end
    task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+     run "touch #{File.join(current_path,'tmp','restart.txt')}"
    end
  end
